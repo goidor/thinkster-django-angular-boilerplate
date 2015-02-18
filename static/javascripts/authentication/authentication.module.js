21e0fbfb5f95b4835a836f8127b2eec7479ed807
Rodrigo Bravo <rodri.brv@gmail.com>
2015-02-17 20:52:20 -0430

    Checkpoint create user (Error status)

---
+++ static/javascripts/authentication/authentication.module.js
@@ -0,0 +1,30 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.authentication', [
+      'thinkster.authentication.controllers',
+      'thinkster.authentication.services'
+    ]);
+
+  angular
+    .module('thinkster.authentication.controllers', []);
+
+  angular
+    .module('thinkster.authentication.services', ['ngCookies']);
+
+  angular
+    .module('thinkster')
+    .run(run);
+
+  run.$inject = ['$http'];
+
+  /**
+  * @name run
+  * @desc Update xsrf $http headers to align with Django's defaults
+  */
+  function run($http) {
+    $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $http.defaults.xsrfCookieName = 'csrftoken';
+  }
+})();
